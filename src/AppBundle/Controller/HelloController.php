30c87f9eff556c72a20d7496f588b1f3b465e8cf
Daniel Hanold <dhanold@dnainfo.com>
2015-04-08 14:51:37 -0400

    Add index controller for app bundle

---
+++ src/AppBundle/Controller/HelloController.php
@@ -0,0 +1,14 @@
+<?php
+
+// Define namespace.
+namespace AppBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class HelloController
+{
+    public function indexAction($name)
+    {
+        return new Response('<html><body>Hello ' . $name . '</body></html>');
+    }
+}
