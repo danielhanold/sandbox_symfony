59c43ddda23834bcf0e4c795cff432ccd3d12e7e
Daniel Hanold <dhanold@dnainfo.com>
2015-04-08 16:28:07 -0400

    Add index page for blog

--- src/AppBundle/Controller/BlogController.php
+++ src/AppBundle/Controller/BlogController.php
@@ -7,6 +7,11 @@ use Symfony\Component\HttpFoundation\Response;
 
 class BlogController extends Controller
 {
+    public function indexAction()
+    {
+        return new Response('<html><body>This is the index page for the blog. Imagine a list of blog posts.</body></html>');
+    }
+
     public function showAction($slug)
     {
         return new Response('<html><body>Hello ' . $slug . '</body></html>');
