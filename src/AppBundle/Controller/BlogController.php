3ede6fbcf7d5c27f2c20877e825718cf7ff73808
Daniel Hanold <dhanold@dnainfo.com>
2015-04-08 17:27:24 -0400

    Add examples for generating paths based on routes

--- src/AppBundle/Controller/BlogController.php
+++ src/AppBundle/Controller/BlogController.php
@@ -14,12 +14,25 @@ class BlogController extends Controller
 
     public function showAction($slug, $_route)
     {
+        // Create an example link to page 2 of the blog listing.
+        $url = $this->get('router')->generate('blog', array(
+            'page' => 2,
+            'category' => 'Symfony'
+        ));
+        var_dump('Generated URL: ' . $url);
+
+        // Create an example link to another blog post.
+        $url_other_post = $this->get('router')->generate('blog_show', array(
+            'slug' => 'another-blog-post'
+        ));
+        var_dump('URI to other blog post: ' . $url_other_post);
+
         // Add some debug information.
-        $url = $this->container->get('request_stack')->getCurrentRequest()->getUri();
-        $uri = $this->container->get('request_stack')->getCurrentRequest()->getRequestUri();
+        $current_url = $this->container->get('request_stack')->getCurrentRequest()->getUri();
+        $current_uri = $this->container->get('request_stack')->getCurrentRequest()->getRequestUri();
         var_dump('Route: ' . $_route);
-        var_dump('URI: ' . $uri);
-        var_dump('Full URL: ' . $url);
+        var_dump('URI: ' . $current_uri);
+        var_dump('Full URL: ' . $current_url);
         return new Response('<html><body>This is a blog page. Hello ' . $slug . '.</body></html>');
     }
 }
