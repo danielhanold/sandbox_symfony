f74b0209460f2d5365f74ec846471fb3b3f69705
Daniel Hanold <dhanold@dnainfo.com>
2015-04-08 17:15:15 -0400

    Add some debug information for blog show action

--- src/AppBundle/Controller/BlogController.php
+++ src/AppBundle/Controller/BlogController.php
@@ -12,8 +12,14 @@ class BlogController extends Controller
         return new Response('<html><body>This is the index page for the blog. Imagine a list of blog posts.<br />The page is ' . $page . '</body></html>');
     }
 
-    public function showAction($slug)
+    public function showAction($slug, $_route)
     {
+        // Add some debug information.
+        $url = $this->container->get('request_stack')->getCurrentRequest()->getUri();
+        $uri = $this->container->get('request_stack')->getCurrentRequest()->getRequestUri();
+        var_dump('Route: ' . $_route);
+        var_dump('URI: ' . $uri);
+        var_dump('Full URL: ' . $url);
         return new Response('<html><body>This is a blog page. Hello ' . $slug . '.</body></html>');
     }
 }
