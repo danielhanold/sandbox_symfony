2774821f1f53105350c2e18b0c396f2d53f03289
Daniel Hanold <dhanold@dnainfo.com>
2015-04-22 13:37:31 -0400

    Load object from database and show on page

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -7,27 +7,51 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use AppBundle\Entity\Product;
 use Symfony\Component\HttpFoundation\Response;
 
-class DefaultController extends Controller {
-  /**
-   * @Route("/app/example", name="homepage")
-   */
-  public function indexAction() {
-    return $this->render('default/index.html.twig');
-  }
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/app/example", name="homepage")
+     */
+    public function indexAction()
+    {
+        return $this->render('default/index.html.twig');
+    }
 
-  /**
-   * Create a product.
-   */
-  public function createAction() {
-    $product = new Product();
-    $product->setName('A Foo Bar');
-    $product->setPrice('19.99');
-    $product->setDescription('Lorem ipsum dolor');
+    /**
+     * Create a product.
+     */
+    public function createAction()
+    {
+        $product = new Product();
+        $product->setName('A Foo Bar');
+        $product->setPrice('19.99');
+        $product->setDescription('Lorem ipsum dolor');
 
-    $em = $this->getDoctrine()->getManager();
-    $em->persist($product);
-    $em->flush();
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($product);
+        $em->flush();
 
-    return new Response('Created product id '.$product->getId());
-  }
+        return new Response('Created product id ' . $product->getId());
+    }
+
+    /**
+     * Load a product.
+     */
+    public function showAction($id)
+    {
+        $product = $this->getDoctrine()
+            ->getRepository('AppBundle:Product')
+            ->find($id);
+
+        if (!$product) {
+            throw $this->createNotFoundException(
+                'No product found for id ' . $id
+            );
+        }
+
+        // Print out the complete object.
+        // d($product);
+
+        return new Response('Found a product for id ' . $id . '. The product is called: ' . $product->getName());
+    }
 }
