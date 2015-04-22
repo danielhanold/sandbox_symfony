9139da3e0b374cf07d228d947c6146a237491f02
Daniel Hanold <danny@danielhanold.com>
2015-04-21 20:44:39 -0400

    Add route and controller to persist products in DB

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,14 +4,30 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use AppBundle\Entity\Product;
+use Symfony\Component\HttpFoundation\Response;
 
-class DefaultController extends Controller
-{
-    /**
-     * @Route("/app/example", name="homepage")
-     */
-    public function indexAction()
-    {
-        return $this->render('default/index.html.twig');
-    }
+class DefaultController extends Controller {
+  /**
+   * @Route("/app/example", name="homepage")
+   */
+  public function indexAction() {
+    return $this->render('default/index.html.twig');
+  }
+
+  /**
+   * Create a product.
+   */
+  public function createAction() {
+    $product = new Product();
+    $product->setName('A Foo Bar');
+    $product->setPrice('19.99');
+    $product->setDescription('Lorem ipsum dolor');
+
+    $em = $this->getDoctrine()->getManager();
+    $em->persist($product);
+    $em->flush();
+
+    return new Response('Created product id '.$product->getId());
+  }
 }
