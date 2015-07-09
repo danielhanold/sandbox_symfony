4538a08546775fb56ed8d706c705b393b4dd52ab
Daniel Hanold <dhanold@dnainfo.com>
2015-07-09 14:19:09 -0400

    Use simpler version to return JSON data

--- src/AppBundle/Controller/LuckyController.php
+++ src/AppBundle/Controller/LuckyController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class LuckyController
 {
@@ -28,10 +29,6 @@ class LuckyController
             'lucky_number' => rand(0, 100),
         );
 
-        return new Response(
-            json_encode($data),
-            200,
-            array('Content-Type' => 'application/json')
-        );
+        return new JsonResponse($data);
     }
 }
